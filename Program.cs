01a845764549b4d6d0884b68fcd47bb06dd290d3
Joe Stead <joestead1@gmail.com>
2016-11-16 17:31:27 +0000

    First example

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var osDescription = RuntimeInformation.FrameworkDescription;
+            Console.WriteLine($"Hello from {osDescription}!");
+        }
+    }
+}
